6cb605df028f1620405a8d9bcfa189657d1e529a
pankaj-prasad <pp5577488@gmail.com>
2020-08-22 17:49:00 +0530

    changes in directory

--- create-env.sh
+++ create-env.sh
@@ -7,7 +7,7 @@ do
 	echo "=============================="
     echo "Creating springdemocontainer$i container.."
     sleep 1
-    docker run -it --name springdemocontainer$i -p 4040:8080 demo/springdemo-img /bin/bash
+    docker run -it --name springdemocontainer$i -p 4040:8080 -d  demo/springdemo-img /bin/bash
     echo "springdemocontainer$i container has been created!"
 	echo "=============================="
 done
